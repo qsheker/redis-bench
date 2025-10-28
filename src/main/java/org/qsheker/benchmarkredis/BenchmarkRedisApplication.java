f36896130fa4e9fe9f029352c88fe0dfbc799d9a
Aldiyar <aldikzhaks@gmail.com>
2025-10-28 23:25:24 +0500

    init commit

---
+++ src/main/java/org/qsheker/benchmarkredis/BenchmarkRedisApplication.java
@@ -0,0 +1,13 @@
+package org.qsheker.benchmarkredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchmarkRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenchmarkRedisApplication.class, args);
+    }
+
+}
